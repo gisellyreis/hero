0130ece7ce2a09a65abdf44782af7b523b566d0d
gisellyreis <gisellyareis@gmail.com>
2020-03-24 19:20:07 -0300

    initial commit

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+
+module.exports = {
+    async index(req, res) {
+        const ongs = await connection('ong').select('*');
+    
+        return res.json(ongs);
+    },
+
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+    
+        const id = crypto.randomBytes(4).toString('HEX');
+    
+        await connection('ong').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+    
+        // console.log(data);
+        return res.json({id});
+    }
+};
